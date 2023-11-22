a762e46baec39b49956fc8babd1eadcb5fe3c8d1
icaro-matheus <146893854+icaro-matheus@users.noreply.github.com>
2023-11-21 21:27:43 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function menuOnClick(){
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
+
